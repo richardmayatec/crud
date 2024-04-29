908c65dac1a3d460eae15a8502f4e712cc7981d4
richardmayatec <127319930+richardmayatec@users.noreply.github.com>
2024-04-29 16:51:41 -0600

    api setup

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\v1\NoteApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/notes', NoteApiController::class);
 });
